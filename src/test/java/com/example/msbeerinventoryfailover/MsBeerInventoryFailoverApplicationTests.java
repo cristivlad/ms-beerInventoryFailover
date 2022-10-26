252091e9f2c6d1399a454bd17e96a6a167a67fa9
cristiii <cristian.vlad10@gmail.com>
2022-10-26 16:30:14 +0300

    init

---
+++ src/test/java/com/example/msbeerinventoryfailover/MsBeerInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msbeerinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
